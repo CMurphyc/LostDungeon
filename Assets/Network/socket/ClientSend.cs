b23acb5aebe380b29e3807a0ae8a4d674ddc0ca0
JerryHu97 <442838203@qq.com>
2020-04-03 14:37:38 +0800

    修改了网络发送协议 统一为 [协议类型4字节] -[包长4字节]- [自定义包体长度]

--- Assets/Network/socket/ClientSend.cs
+++ Assets/Network/socket/ClientSend.cs
@@ -13,8 +13,33 @@ public class ClientSend
     {
         remoteServer = sock;
     }
-    void Send(byte[] data)
+    //void Send(byte[] data)
+    //{
+    //    if (remoteServer.Connected)
+    //    {
+
+    //        NetworkStream ns = remoteServer.GetStream();
+    //        if (ns.CanWrite)
+    //        {
+    //            ns.Write(data, 0, data.Length);
+    //        }
+    //    }
+    //}
+
+    void Send(byte[] data, int ProtoType)
     {
+        byte[] init = PackConverter.intToBytes(ProtoType);
+        byte[] databytes = data;
+        byte[] length_pack = PackConverter.intToBytes(databytes.Length);
+
+
+        byte[] final = new byte[databytes.Length + init.Length + length_pack.Length];
+        //pack.CalculateSize()
+
+        init.CopyTo(final, 0);
+        length_pack.CopyTo(final, init.Length);
+        databytes.CopyTo(final, init.Length + length_pack.Length);
+
         if (remoteServer.Connected)
         {
 
@@ -24,6 +49,7 @@ public class ClientSend
                 ns.Write(data, 0, data.Length);
             }
         }
+
     }
     public void RegisterC2S(string UserName, string Passward, string NickName)
     {
@@ -32,12 +58,7 @@ public class ClientSend
         pack.Username = UserName;
         pack.Password = Passward;
         pack.Nickname = NickName;
-        byte[] databytes = pack.ToByteArray();
-        byte[] init = PackConverter.intToBytes(GeneralType.UserRegister);
-        byte[] final = new byte[databytes.Length + init.Length];
-        init.CopyTo(final, 0);
-        databytes.CopyTo(final, init.Length);
-        Send(final);
+        Send(pack.ToByteArray(), GeneralType.UserLogin);
 
     }
     public void LoginC2S(string UserName, string Passward)
@@ -45,73 +66,8 @@ public class ClientSend
         LoginC2S pack = new LoginC2S();
         pack.Username = UserName;
         pack.Password = Passward;
-        byte[] databytes = pack.ToByteArray();
-        byte[] init = PackConverter.intToBytes(GeneralType.UserLogin);
-        byte[] final = new byte[databytes.Length + init.Length];
-        init.CopyTo(final, 0);
-        databytes.CopyTo(final, init.Length);
-        Send(final);
+        Send(pack.ToByteArray(), GeneralType.UserRegister);
     }
-    public void CreateRoomC2S(string username,int roomsize)
-    {
-        CreateRoomC2S pack = new CreateRoomC2S();
-        pack.Owner = username;
-        pack.Roomsize = roomsize;
-        byte[] databytes = pack.ToByteArray();
-        byte[] init = PackConverter.intToBytes(GeneralType.CreateGame);
-        byte[] final = new byte[databytes.Length + init.Length];
-        init.CopyTo(final, 0);
-        databytes.CopyTo(final, init.Length);
-        Send(final);
-    }
-    public void GetRoomListC2S()
-    {
-        GetRoomListC2S pack = new GetRoomListC2S();
-        byte[] databytes = pack.ToByteArray();
-        byte[] init = PackConverter.intToBytes(GeneralType.GetRoomList);
-        byte[] final = new byte[databytes.Length + init.Length];
-        init.CopyTo(final, 0);
-        databytes.CopyTo(final, init.Length);
-        Send(final);
-
-    }
-    public void EnterRoomC2S(string username, int roomid)
-    {
-        EnterRoomC2S pack = new EnterRoomC2S();
-        pack.Username = username;
-        pack.Roomid = roomid;
-        byte[] databytes = pack.ToByteArray();
-        byte[] init = PackConverter.intToBytes(GeneralType.EnterRoom);
-        byte[] final = new byte[databytes.Length + init.Length];
-        init.CopyTo(final, 0);
-        databytes.CopyTo(final, init.Length);
-        Send(final);
-    }
-
-    public void PlayerReadyC2S(string username , int status)
-    {
-        PlayerReadyC2S pack = new PlayerReadyC2S();
-        pack.Username = username;
-        pack.Status = status;
-        byte[] databytes = pack.ToByteArray();
-        byte[] init = PackConverter.intToBytes(GeneralType.PlayerReady);
-        byte[] final = new byte[databytes.Length + init.Length];
-        init.CopyTo(final, 0);
-        databytes.CopyTo(final, init.Length);
-        Send(final);
-    }
-
-    public void RoomOwnerStartC2S(string username)
-    {
-        RoomOwnerStartC2S pack = new RoomOwnerStartC2S();
-        pack.Ownerid = username;
-        
-        byte[] databytes = pack.ToByteArray();
-        byte[] init = PackConverter.intToBytes(GeneralType.RoomOwnerStart);
-        byte[] final = new byte[databytes.Length + init.Length];
-        init.CopyTo(final, 0);
-        databytes.CopyTo(final, init.Length);
-        Send(final);
-    }
+  
 
 }
