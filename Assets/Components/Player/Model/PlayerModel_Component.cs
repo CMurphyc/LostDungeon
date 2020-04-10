87dc43525a0b3b26d298f2b28c86c17f8e4ee069
maxding <maxding@123u.com>
2020-04-10 14:43:20 +0800

    新增测试

--- Assets/Components/Player/Model/PlayerModel_Component.cs
+++ Assets/Components/Player/Model/PlayerModel_Component.cs
@@ -4,7 +4,31 @@ using UnityEngine;
 
 public class PlayerModel_Component : MonoBehaviour
 {
+
     FixVector3 Position;
     FixVector3 Rotation;
     Fix64 HP;
+    int Frame;
+    void Awake()
+    {
+        Position = new FixVector3((Fix64)(-4),(Fix64)1,(Fix64)0);
+    }
+
+    public FixVector3 GetPosition()
+    {
+        return Position;
+    }
+    public FixVector3 GetRotation()
+    {
+        return Rotation;
+    }
+    public void Move(Vector2 v)
+    {
+        Position.x += v.x;
+        Position.y += v.y;
+    }
+    public int GetFrame()
+    {
+        return Frame;
+    }
 }
--- Assets/Components/Player/View/PlayerView_Component.cs
+++ Assets/Components/Player/View/PlayerView_Component.cs
@@ -1,11 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerView_Component : Component
+public class PlayerView_Component : MonoBehaviour
 {
+    GameObject main;
+    private Animator ani;
+    void Start()
+    {
+        ani =GetComponent<Animator>();
+        //main = GameObject.FindWithTag("GameEntry");
+    }
+    void Update()
+    {
+        RefreshView();
+    }
     public void RefreshView()
     {
-
-
+        UpdatePosition();
+    }
+    private void UpdatePosition()
+    {
+        FixVector3 FixPos=gameObject.GetComponent<PlayerModel_Component>().GetPosition();
+        Vector3 FloatPos = new Vector3((float)FixPos.x, (float)FixPos.y, (float)FixPos.z);
+        //FloatPos= main.GetComponent<GameMain>().WorldSystem._model.
+        transform.position = FloatPos;
+        CameraManager.instance.transform.position = new Vector3(FloatPos.x, FloatPos.y, -2);
+    }
+    public void Play(string animation)
+    {
+        ani.Play(animation);
     }
 }
