cfac7632860e69878acf766221409200fa78210e
stupid-turtle <lyh.stupidturtle@gmail.com>
2020-04-16 19:36:32 +0800

    添加道具和装备素材，添加人物基类

--- Assets/Components/Player/Model/PlayerModel_Component.cs
+++ Assets/Components/Player/Model/PlayerModel_Component.cs
@@ -4,36 +4,119 @@ using UnityEngine;
 
 public class PlayerModel_Component : MonoBehaviour
 {
+    Fix64 healthPoint;   // 玩家血量
+    Fix64 playerSpeed;   // 玩家移动速度
+    Fix64 attackPoint;   // 玩家攻击力
+    FixVector3 playerPosition;   // 玩家位置
+    bool playerRotation;   // 玩家朝向
+    FixVector3 weaponPosition;   // 武器位置
+    FixVector3 weaponRotation;   // 武器朝向
+    Fix64 bulletSpeed;   // 玩家射出子弹的速度
+    Fix64 roomId;   // 玩家所处的房间ID
 
-    FixVector3 Position;
-    bool Rotation;
-    Fix64 HP;
-    int Frame;
     void Awake()
     {
         //Position = new FixVector3((Fix64)(-4),(Fix64)1,(Fix64)0);
-        Position = new FixVector3((Fix64)transform.position.x, (Fix64)transform.position.y, (Fix64)transform.position.z);
+        playerPosition = new FixVector3((Fix64)transform.position.x, (Fix64)transform.position.y, (Fix64)transform.position.z);
     }
 
-    public Vector3 GetPosition()
+    public void SetHealthPoint(Fix64 _healthPoint)
     {
-        return new Vector3((float)Position.x,(float)Position.y,(float)Position.z);
+        healthPoint = _healthPoint;
     }
-    public bool GetRotation()
+
+    public Fix64 GetHealthPoint()
     {
-        return Rotation;
+        return healthPoint;
     }
+
+    public void SetPlayerSpeed(Fix64 _playerSpeed)
+    {
+        playerSpeed = _playerSpeed;
+    }
+
+    public Fix64 GetPlayerSpeed()
+    {
+        return playerSpeed;
+    }
+
+    public void SetAttackPoint(Fix64 _attackPoint)
+    {
+        attackPoint = _attackPoint;
+    }
+
+    public Fix64 GetAttackPoint()
+    {
+        return attackPoint;
+    }
+
+    public void SetPlayerPosition(FixVector3 _playerPosition)
+    {
+        playerPosition = _playerPosition;
+    }
+
+    public FixVector3 GetPlayerPosition()
+    {
+        return playerPosition;
+    }
+
+    public void SetPlayerRotation(bool _playerRotation)
+    {
+        playerRotation = _playerRotation;
+    }
+
+    public bool GetPlayerRotation()
+    {
+        return playerRotation;
+    }
+
+    public void SetWeaponPosition(FixVector3 _weaponPosition)
+    {
+        weaponPosition = _weaponPosition;
+    }
+
+    public FixVector3 GetWeaponPosition()
+    {
+        return weaponPosition;
+    }
+
+    public void SetWeaponRotation(FixVector3 _weaponRotation)
+    {
+        weaponRotation = _weaponRotation;
+    }
+
+    public FixVector3 GetWeaponRotation()
+    {
+        return weaponRotation;
+    }
+
+    public void SetBulletSpeed(Fix64 _bulletSpeed)
+    {
+        bulletSpeed = _bulletSpeed;
+    }
+
+    public Fix64 GetBulletSpeed()
+    {
+        return bulletSpeed;
+    }
+
+    public void SetRoomId(Fix64 _roomId)
+    {
+        roomId = _roomId;
+    }
+
+    public Fix64 GetRoomId()
+    {
+        return roomId;
+    }
+
     public void Move(Vector2 v)
     {
-        Position.x += v.x;
-        Position.y += v.y;
+        playerPosition.x += v.x;
+        playerPosition.y += v.y;
         if (v.x != 0)
         {
-            Rotation = v.x < 0 ? true : false;
+            playerRotation = v.x < 0 ? true : false;
         }
-    }
-    public int GetFrame()
-    {
-        return Frame;
     }
 }
--- Assets/Components/Player/View/PlayerView_Component.cs
+++ Assets/Components/Player/View/PlayerView_Component.cs
@@ -3,33 +3,45 @@ using System.Collections;
 
 public class PlayerView_Component : MonoBehaviour
 {
-    GameObject main;
-    private Animator ani;
+    GameObject player;
+    private Animator anim;
+
     void Start()
     {
-        ani =GetComponent<Animator>();
+        anim = GetComponent<Animator>();
     }
+
     void Update()
     {
         RefreshView();
     }
+
     public void RefreshView()
     {
         UpdatePosition();
         UpdateRotation();
     }
+
     private void UpdatePosition()
     {
-        Vector3 FloatPos = GetComponent<PlayerModel_Component>().GetPosition();
-        transform.position = FloatPos;
-        
+        FixVector3 FloatPos = GetComponent<PlayerModel_Component>().GetPlayerPosition();
+        if (FloatPos == new FixVector3((Fix64)transform.position.x, (Fix64)transform.position.y, (Fix64)transform.position.z)) SetRun(false);
+        else SetRun(true);
+        transform.position = new Vector3((float)FloatPos.x, (float)FloatPos.y, (float)FloatPos.z);
     }
+
     private void UpdateRotation()
     {
-        GetComponent<SpriteRenderer>().flipX = GetComponent<PlayerModel_Component>().GetRotation();
+        GetComponent<SpriteRenderer>().flipX = GetComponent<PlayerModel_Component>().GetPlayerRotation();
     }
+
     public void Play(string animation)
     {
-        ani.Play(animation);
+        anim.Play(animation);
+    }
+
+    public void SetRun(bool state)
+    {
+        anim.SetBool("IsRun", state);
     }
 }
