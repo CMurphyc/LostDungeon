3327e89b990180d9d8a149c2ea3ca3bd0ae9e8a4
x10 <lin5597@foxmail.com>
2020-04-29 21:58:12 +0800

    传送下一层

--- Assets/Manager/System/BattleModule/TerrainModule.cs
+++ Assets/Manager/System/BattleModule/TerrainModule.cs
@@ -105,6 +105,9 @@ public class TerrainModule
                     if (!InGateRange)
                     {
                         // to do 
+                       GameObject.FindWithTag("GameEntry").GetComponent<GameMain>().socket.sock_c2s.NextFloor(
+                           _parentManager.sys._model._RoomModule.MapFloorNumber
+                        );
                         Debug.Log("去下一层了！");
                         //发送传送下一层请求
                         InGateRange = true;
