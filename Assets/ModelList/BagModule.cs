212a2e4ad08ebe47f4ad8732485d1fc064d6a88a
JerryHu97 <442838203@qq.com>
2020-05-08 14:45:29 +0800

    背包model  + 战斗UI技能按钮适配
    Former-commit-id: 36d293eba4eb3e81fb77c52e1bf927be489c617e

--- Assets/ModelList/BagModule.cs
+++ Assets/ModelList/BagModule.cs
@@ -2,17 +2,76 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public struct Item
+public class Item
 {
+    //道具ID
     public int ItemID;
-
+    //道具数量
     public int ItemNumber;
 
 }
 public class BagModule  
 {
-    //队友背包
-    public Dictionary<string, List<Item>> TeamPlayerBag;
-    //当前玩家
-    public List<Item> PlayerBag;
+
+    public Dictionary<int, List<Item>> PlayerBag = new Dictionary<int, List<Item>>();
+
+
+    public void Free()
+    {
+        PlayerBag.Clear();
+    }
+
+    //添加道具
+    public void AddItem(int UID, Item it)
+    {
+        if (!PlayerBag.ContainsKey(UID))
+        {
+            List<Item> newList = new List<Item>();
+            PlayerBag.Add(UID, newList);
+        }
+        else
+        {
+            if (CheckItemExit(PlayerBag[UID], it.ItemID))
+            {
+                int ItemIndex = GetItemIndex(PlayerBag[UID], it.ItemID);
+                if (ItemIndex != -1)
+                {
+                    PlayerBag[UID][ItemIndex].ItemNumber++;
+                }
+            }
+            else
+            {
+                PlayerBag[UID].Add(it);
+            }
+        }
+    }
+
+    bool CheckItemExit(List<Item> it, int ItemID)
+    {
+        for (int i =0;i< it.Count;i++)
+        {
+            if (it[i].ItemID == ItemID)
+            {
+
+                return true;
+            }
+        }
+        return false;
+    }
+
+    int GetItemIndex (List<Item> it, int ItemID)
+    {
+        for (int i = 0; i < it.Count; i++)
+        {
+            if (it[i].ItemID == ItemID)
+            {
+
+                return i;
+            }
+        }
+        return -1;
+
+    }
 }
+
+
