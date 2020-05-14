a77c8577d2a392d1b1f1da1bf351622d14e06b88
Mitsuha <276091871@qq.com>
2020-05-14 18:27:36 +0800

    Main场景UI
    Former-commit-id: e16f3ae92495f48e4e3ef3306e91246476c34575

---
+++ Assets/UI/Main/HeroChange.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeroChange : MonoBehaviour
+{
+    Button btnLeft;
+    Button btnRight;
+    Vector3 LeftPosition;
+    Vector3 RightPosition;
+    public GameObject[] Hero;
+    private int NowHeroIndex=0;
+    private Vector3 NowHeroPosition;
+    private GameObject NowHeroObj;
+    private Text HeroName;
+    private void Awake()
+    {
+        HeroName = transform.Find("Text").GetComponent<Text>();
+        LeftPosition = transform.Find("LeftPosition").position;
+        RightPosition = transform.Find("RightPosition").position;
+        btnLeft = transform.Find("btnLeft").GetComponent<Button>();
+        btnRight = transform.Find("btnRight").GetComponent<Button>();
+        btnLeft.onClick.AddListener(OnbtnLeft);
+        btnRight.onClick.AddListener(OnbtnRight);
+    }
+
+    private void Update()
+    {
+        NowHeroPosition = Vector3.Lerp(NowHeroPosition,transform.position,0.05f);
+        NowHeroObj.transform.position = NowHeroPosition;
+    }
+
+    void OnbtnLeft()
+    {
+        NowHeroIndex=(NowHeroIndex+1+Hero.Length)%Hero.Length;
+        NowHeroObj = Hero[NowHeroIndex];
+        NowHeroPosition = RightPosition;
+        NowHeroObj.transform.position = NowHeroPosition;
+        HeroName.text = NowHeroObj.name;
+    }
+    void OnbtnRight()
+    {
+        NowHeroIndex = (NowHeroIndex - 1 + Hero.Length) % Hero.Length;
+        NowHeroObj = Hero[NowHeroIndex];
+        NowHeroPosition = LeftPosition;
+        NowHeroObj.transform.position = NowHeroPosition;
+        HeroName.text = NowHeroObj.name;
+    }
+}
