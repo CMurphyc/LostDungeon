1944c845bfc0ab5c033a5af579d9979cb899f21a
Mitsuha <276091871@qq.com>
2020-05-14 19:16:49 +0800

    Main UI界面调整
    Former-commit-id: 708f382a5f9b84d64f710ecb80e7757aeba4c6d3

--- Assets/UI/Main/HeroChange.cs
+++ Assets/UI/Main/HeroChange.cs
@@ -10,12 +10,22 @@ public class HeroChange : MonoBehaviour
     Vector3 LeftPosition;
     Vector3 RightPosition;
     public GameObject[] Hero;
+    private List<GameObject> NewHero=new List<GameObject>();
     private int NowHeroIndex=0;
     private Vector3 NowHeroPosition;
     private GameObject NowHeroObj;
     private Text HeroName;
     private void Awake()
     {
+        for(int i=0;i<4;i++)
+        {
+            if (Hero[i] == null) Debug.LogError("??");
+            NewHero.Add(Object.Instantiate(Hero[i],new Vector3(0,0,0),Quaternion.identity,transform) as GameObject);
+            NewHero[i].transform.localScale = new Vector3(400, 400, 0);
+
+            for(int j = 0; j < NewHero[i].transform.childCount; ++j) NewHero[i].transform.GetChild(j).GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 0);
+            NewHero[i].GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 0);
+        }
         HeroName = transform.Find("Text").GetComponent<Text>();
         LeftPosition = transform.Find("LeftPosition").position;
         RightPosition = transform.Find("RightPosition").position;
@@ -23,26 +33,37 @@ public class HeroChange : MonoBehaviour
         btnRight = transform.Find("btnRight").GetComponent<Button>();
         btnLeft.onClick.AddListener(OnbtnLeft);
         btnRight.onClick.AddListener(OnbtnRight);
+
+        NowHeroObj = NewHero[NowHeroIndex];
     }
 
     private void Update()
     {
-        NowHeroPosition = Vector3.Lerp(NowHeroPosition,transform.position,0.05f);
+        NowHeroPosition = Vector3.Lerp(NowHeroPosition,transform.position,0.2f);
         NowHeroObj.transform.position = NowHeroPosition;
+        NowHeroObj.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1 - Mathf.Abs(NowHeroPosition.x - transform.position.x));
+        for (int j = 0; j < NowHeroObj.transform.childCount; ++j) NowHeroObj.transform.GetChild(j).GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1 - Mathf.Abs(NowHeroPosition.x - transform.position.x));
     }
 
+    private void ClearColorAlpha(GameObject obj)
+    {
+        for (int j = 0; j < obj.transform.childCount; ++j) obj.transform.GetChild(j).GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 0);
+        obj.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 0);
+    }
     void OnbtnLeft()
     {
+        ClearColorAlpha(NowHeroObj);
         NowHeroIndex=(NowHeroIndex+1+Hero.Length)%Hero.Length;
-        NowHeroObj = Hero[NowHeroIndex];
+        NowHeroObj = NewHero[NowHeroIndex];
         NowHeroPosition = RightPosition;
         NowHeroObj.transform.position = NowHeroPosition;
         HeroName.text = NowHeroObj.name;
     }
     void OnbtnRight()
     {
+        ClearColorAlpha(NowHeroObj);
         NowHeroIndex = (NowHeroIndex - 1 + Hero.Length) % Hero.Length;
-        NowHeroObj = Hero[NowHeroIndex];
+        NowHeroObj = NewHero[NowHeroIndex];
         NowHeroPosition = LeftPosition;
         NowHeroObj.transform.position = NowHeroPosition;
         HeroName.text = NowHeroObj.name;
